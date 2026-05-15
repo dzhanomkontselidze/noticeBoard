0cc7cb35e12395f2680f050cf130337c462c549c
Yulia Hoisan <hoisan.v.yulia@ukd.edu.ua>
2026-05-15 21:34:52 +0300

    Update connector.js

--- db/connector.js
+++ db/connector.js
@@ -9,19 +9,32 @@ const pool = new Pool({
         rejectUnauthorized: false
     }
 });
+
 const createTableQueries = [];
-createTableQueries.push(`CREATE TABLE IF NOT EXISTS notices (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    author TEXT NOT NULL,
-    content TEXT,
-    importance INTEGER DEFAULT 0 CHECK (importance >= 0 AND importance <= 10),
-    start_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP NOT NULL,
-    status TEXT DEFAULT 'active' CHECK (status IN ('active', 'archived')),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+createTableQueries.push(`
+    CREATE TABLE IF NOT EXISTS profiles (
+        id SERIAL PRIMARY KEY,
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
-        `);
+`);
+
+createTableQueries.push(`
+    CREATE TABLE IF NOT EXISTS user_notices (
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        author_id INTEGER REFERENCES profiles(id) ON DELETE CASCADE, 
+        content TEXT,
+        importance INTEGER DEFAULT 0 CHECK (importance >= 0 AND importance <= 10),
+        start_date TIMESTAMP NOT NULL,
+        end_date TIMESTAMP NOT NULL,
+        status TEXT DEFAULT 'active' CHECK (status IN ('active', 'archived')),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+`);
 for await (const query of createTableQueries) {
     try {
         console.log(query.slice(0, query.indexOf('(')).trim() + "...")
